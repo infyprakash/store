64419c237d4d327042252dbddb0a3bf819e26525
infyprakash <infymee@gmail.com>
2022-06-27 17:34:34 +0545

    'changes'

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -140,7 +140,7 @@ WSGI_APPLICATION = 'marketplace.wsgi.application'
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'HOST': '127.0.0.1',
+            'HOST': '/cloudsql/ecomdb-002',
             'PORT': '3306',
             'NAME': 'ecomdb',
             'USER': 'root',
