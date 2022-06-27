26d4fa2390a7e1b922e7e96c82af13d30bf0f1bc
infyprakash <infymee@gmail.com>
2022-06-27 17:14:59 +0545

    'changes'

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -130,12 +130,24 @@ WSGI_APPLICATION = 'marketplace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'NAME': 'ecomdb',
+            'USER': 'root',
+            'PASSWORD': '',
+        }
     }
-}
+
 
 
 # Password validation
