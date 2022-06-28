8d643a9e85d9ce3a581d0e40da542b7d21290dba
infyprakash <infymee@gmail.com>
2022-06-28 13:01:28 +0545

    setting change

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -219,9 +219,9 @@ STATIC_ROOT = 'static'
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
